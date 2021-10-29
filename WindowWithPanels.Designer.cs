2d21aba7ccddbd005b4ce9c0725961e4339a4c94
Сергей <malahovsv1999@gmail.com>
2021-10-29 18:02:04 +0700

    First commit

---
+++ WindowWithPanels.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WorkWithPanels
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SetPanels = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // SetPanels
+            // 
+            this.SetPanels.AutoScroll = true;
+            this.SetPanels.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SetPanels.Location = new System.Drawing.Point(0, 0);
+            this.SetPanels.Name = "SetPanels";
+            this.SetPanels.Size = new System.Drawing.Size(800, 450);
+            this.SetPanels.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SetPanels);
+            this.Name = "Form1";
+            this.Text = "Работаю с панельками";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel SetPanels;
+    }
+}
+
---
+++ WindowWithPanels.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkWithPanels
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Просто череда вызова метода createPanel() который возвращает объект панель и помещает его в контейнер
+            SetPanels.Controls.Add(createPanel("Панель 1", "Panel1"));
+            SetPanels.Controls.Add(createPanel("Панель 2", "Panel2"));
+            SetPanels.Controls.Add(createPanel("Панель 3", "Panel3"));
+            SetPanels.Controls.Add(createPanel("Панель 4", "Panel4"));
+            SetPanels.Controls.Add(createPanel("Панель 5", "Panel5"));
+            SetPanels.Controls.Add(createPanel("Панель 6", "Panel6"));
+            SetPanels.Controls.Add(createPanel("Панель 7", "Panel7"));
+
+        }
+
+        /// <summary>
+        /// Создание панели
+        /// </summary>
+        /// <param name="text">Текст размещающийся на панели</param>
+        /// <param name="namePanel">Имя панели</param>
+        /// <returns>Возвращает полностью собранную панель, которая даже имеет имя</returns>
+        private Panel createPanel(string text, string namePanel)
+        {
+            var newPanel = new Panel
+            {
+                Width = 200,
+                Height = 200,
+                BackColor = Color.Gray,
+                Name = $"{namePanel}"
+            };
+            newPanel.Controls.Add(createHeaderPanel(newPanel));
+            installEventsPanel(newPanel);
+            return newPanel;
+        }
+
+        private Label createHeaderPanel(Panel panel)
+        {
+            var label = new Label()
+            {
+                Text = panel.Name,
+                TextAlign = ContentAlignment.MiddleCenter,
+                Font = new Font("Consolas", 12, FontStyle.Bold),
+                Dock = DockStyle.Fill,
+                ForeColor = Color.White
+            };
+            installEventsLabel(label, panel);
+            return label;
+        }
+
+        private void installEventsPanel(Panel panel)
+        {
+            panel.Click += (s, e) => { MessageBox.Show(panel.Name); };
+            panel.MouseEnter += (s, e) => { panel.BackColor = Color.Aqua; };
+            panel.MouseLeave += (s, e) => { panel.BackColor = Color.Gray; };
+        }
+
+        /// <summary>
+        /// Присваиваем события дочернему Label
+        /// </summary>
+        /// <param name="label">Дочерний Label</param>
+        /// <param name="panel">Родительский Panel</param>
+        private void installEventsLabel (Label label, Panel panel)
+        {
+            label.Click += (s, e) => { MessageBox.Show(panel.Name); };
+            label.MouseEnter += (s, e) => { panel.BackColor = Color.Aqua; };
+            label.MouseLeave += (s, e) => { panel.BackColor = Color.Gray; };
+        }
+    }
+}
